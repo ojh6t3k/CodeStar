49855215aa556f385ab89613e07b65b9597f787a
Jaehong, Oh <ojh6t3k@gmail.com>
2017-05-30 10:15:32 +0900

    통신 테스트 프로그램 완료

---
+++ Assets/CodeStar/Scripts/MonoBehaviours/CodeStarTest.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Makist;
+
+public class CodeStarTest : MonoBehaviour
+{
+	public Toggle tourMode;
+	public Toggle correctMode;
+	public Toggle incorrectMode;
+	public Dropdown velocityList;
+	public GameObject completeMessage;
+
+	private SerialComm _serial;
+
+	void Awake()
+	{
+		completeMessage.SetActive(false);
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+		_serial = FindObjectOfType<SerialComm>();
+		if(_serial == null)
+			Debug.LogError("Can not find SerialComm!");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if(_serial.IsOpen)
+		{
+			byte[] data = _serial.Read();
+			if(data != null)
+			{
+				for(int i = 0; i < data.Length; i++)
+				{
+					if(data[i] == 33)
+					{
+						completeMessage.SetActive(true);
+						break;
+					}
+				}
+			}
+		}
+	}
+
+	public void Cmd_Go()
+	{
+		if(_serial.IsOpen)
+		{
+			byte data = 32;
+			if(tourMode.isOn)
+				data += 1;
+			else if(correctMode.isOn)
+				data += 2;
+			else if(incorrectMode.isOn)
+				data += 3;
+			_serial.Write(new byte[] { data });
+		}
+	}
+
+	public void Cmd_Velocity()
+	{
+		if(_serial.IsOpen)
+		{
+			byte data = 48;
+			data += (byte)velocityList.value;
+			_serial.Write(new byte[] { data });
+		}
+	}
+
+	public void Cmd_Home()
+	{
+		if(_serial.IsOpen)
+		{
+			byte data = 64;
+			_serial.Write(new byte[] { data });
+		}
+	}
+}
---
+++ Assets/CodeStar/Scripts/MonoBehaviours/ConnectionUI.cs
@@ -0,0 +1,181 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Makist;
+
+
+public class ConnectionUI : MonoBehaviour
+{
+	public SerialComm serialComm;
+	public Button connect;
+	public Button disconnect;
+	public Button quit;
+	public Canvas popupCanvas;
+	public RectTransform settingCommSocket;
+	public Dropdown portList;
+	public Button okCommSocket;
+	public Button cancelCommSocket;
+	public Canvas messageCanvas;
+	public RectTransform msgConnecting;
+	public RectTransform msgConnectionFailed;
+	public RectTransform msgLostConnection;
+	public Button okConnectionFailed;
+	public Button okLostConnection;
+	public GameObject appUI;
+
+
+	void Awake()
+	{
+		serialComm.OnOpen.AddListener(OnSerialOpen);
+		serialComm.OnClose.AddListener(OnSerialClose);
+		serialComm.OnOpenFailed.AddListener(OnSerialOpenFailed);
+		serialComm.OnErrorClosed.AddListener(OnSerialErrorClosed);
+		serialComm.OnStartSearch.AddListener(OnSerialStartSearch);
+		serialComm.OnFoundDevice.AddListener(OnSerialFoundDevice);
+		serialComm.OnStopSearch.AddListener(OnSerialStopSearch);
+
+		connect.onClick.AddListener(OnConnectClick);
+		disconnect.onClick.AddListener(OnDisconnectClick);
+		quit.onClick.AddListener(OnQuitClick);
+		okConnectionFailed.onClick.AddListener(OnMessageOKClick);
+		okLostConnection.onClick.AddListener(OnMessageOKClick);
+		okCommSocket.onClick.AddListener(OnCommSocketOKClick);
+		cancelCommSocket.onClick.AddListener(OnCommSocketCancelClick);
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+		connect.gameObject.SetActive(true);
+		disconnect.gameObject.SetActive(false);
+		popupCanvas.gameObject.SetActive(false);
+		settingCommSocket.gameObject.SetActive(false);
+		messageCanvas.gameObject.SetActive(false);
+		appUI.gameObject.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	#region EventHandler
+	private void OnSerialOpen()
+	{
+		disconnect.gameObject.SetActive(true);
+		connect.gameObject.SetActive(false);
+		messageCanvas.gameObject.SetActive(false);
+		msgConnecting.gameObject.SetActive(false);
+		msgConnectionFailed.gameObject.SetActive(false);
+		msgLostConnection.gameObject.SetActive(false);
+		appUI.gameObject.SetActive(true);
+	}
+
+	private void OnSerialClose()
+	{
+		disconnect.gameObject.SetActive(false);
+		connect.gameObject.SetActive(true);
+		appUI.gameObject.SetActive(false);
+	}
+
+	private void OnSerialOpenFailed()
+	{
+		messageCanvas.gameObject.SetActive(true);
+		msgConnecting.gameObject.SetActive(false);
+		msgConnectionFailed.gameObject.SetActive(true);
+		msgLostConnection.gameObject.SetActive(false);
+	}
+
+	private void OnSerialErrorClosed()
+	{
+		messageCanvas.gameObject.SetActive(true);
+		msgConnecting.gameObject.SetActive(false);
+		msgConnectionFailed.gameObject.SetActive(false);
+		msgLostConnection.gameObject.SetActive(true);
+	}
+
+	private void OnSerialStartSearch()
+	{		
+	}
+
+	private void OnSerialStopSearch()
+	{
+		for(int i=0; i<serialComm.foundDevices.Count; i++)
+		{
+			if(serialComm.device.Equals(serialComm.foundDevices[i]))
+			{
+				if(portList.value == i)
+					portList.captionText.text = serialComm.device.name;
+				else
+					portList.value = i;
+
+				return;
+			}
+		}
+
+		if(serialComm.foundDevices.Count > 0)
+			portList.captionText.text = portList.options[0].text;
+		else
+		{
+			portList.captionText.text = "";
+			okCommSocket.interactable = false;
+		}
+	}
+
+	private void OnSerialFoundDevice(CommDevice device)
+	{
+		Dropdown.OptionData item = new Dropdown.OptionData();
+		item.text = device.name;
+		portList.options.Add(item);
+	}
+
+	private void OnConnectClick()
+	{
+		popupCanvas.gameObject.SetActive(true);
+		settingCommSocket.gameObject.SetActive(true);
+		okCommSocket.interactable = true;
+		portList.options.Clear();
+
+		serialComm.StartSearch();
+	}
+
+	private void OnDisconnectClick()
+	{
+		serialComm.Close();
+	}
+
+	private void OnQuitClick()
+	{
+		serialComm.Close();
+		Application.Quit();
+	}
+
+	private void OnMessageOKClick()
+	{
+		messageCanvas.gameObject.SetActive(false);
+	}
+
+	private void OnCommSocketOKClick()
+	{
+		if(portList.options.Count > 0)
+			serialComm.device = new CommDevice(serialComm.foundDevices[portList.value]);
+
+		serialComm.Open();
+
+		popupCanvas.gameObject.SetActive(false);
+		settingCommSocket.gameObject.SetActive(false);
+		messageCanvas.gameObject.SetActive(true);
+		msgConnecting.gameObject.SetActive(true);
+		msgConnectionFailed.gameObject.SetActive(false);
+		msgLostConnection.gameObject.SetActive(false);
+	}
+
+	private void OnCommSocketCancelClick()
+	{
+		popupCanvas.gameObject.SetActive(false);
+		settingCommSocket.gameObject.SetActive(false);
+	}
+	#endregion
+}
