5357313bff69893cac27fc7380e3e76156000c84
Jaehong, Oh <ojh6t3k@gmail.com>
2017-05-31 16:15:37 +0900

    UI 샘플 데모 제작중

---
+++ Assets/CodeStar/Scripts/MonoBehaviours/DragItem.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(RectTransform))]
+public class DragItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+	public Canvas rootCanvas;
+	public Transform dragRoot;
+	public DragSlot targetSlot;
+	#if PLAYMAKER
+	public string eventName = "DROP ITEM";
+	#endif
+
+	private RectTransform _rectTransform;
+	private Transform _parent;
+	private Vector2 _startPosition;
+	private Vector3 _dragPosition;
+
+	void Awake()
+	{
+		_rectTransform = GetComponent<RectTransform>();
+		_startPosition = _rectTransform.anchoredPosition;
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	public void OnBeginDrag (PointerEventData eventData)
+	{
+		_parent = transform.parent;
+		_rectTransform.SetParent(dragRoot);
+
+		if(targetSlot != null)
+			targetSlot.SetDropItem(this);
+	}
+
+	public void OnDrag (PointerEventData eventData)
+	{
+		_dragPosition = Input.mousePosition;
+
+		if(rootCanvas.renderMode == RenderMode.ScreenSpaceCamera)
+		{
+			_dragPosition.z = rootCanvas.planeDistance;
+			_dragPosition = Camera.main.ScreenToWorldPoint(_dragPosition);
+		}
+		
+		transform.position = _dragPosition;
+	}
+
+	public void OnEndDrag (PointerEventData eventData)
+	{
+		_rectTransform.SetParent(_parent);
+		_rectTransform.anchoredPosition = _startPosition;
+	}
+}
---
+++ Assets/CodeStar/Scripts/MonoBehaviours/DragSlot.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+#if PLAYMAKER
+using HutongGames.PlayMaker;
+#endif
+
+[RequireComponent(typeof(RectTransform))]
+public class DragSlot : MonoBehaviour, IDropHandler
+{
+	#if PLAYMAKER
+	public PlayMakerFSM targetFSM;
+	#endif
+
+	private DragItem _item;
+
+	void Awake()
+	{
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	public void OnDrop(PointerEventData eventData)
+	{
+		if(_item == null)
+			return;
+
+		#if PLAYMAKER
+		if(targetFSM != null)
+		{
+			FsmEventTarget fsmEventTarget = new FsmEventTarget();
+			fsmEventTarget.target = FsmEventTarget.EventTarget.FSMComponent;
+			fsmEventTarget.fsmComponent = targetFSM;
+
+			targetFSM.Fsm.Event(fsmEventTarget, _item.eventName);
+		}
+		#endif
+
+		_item.gameObject.SetActive(false);
+		_item = null;
+	}
+
+	public void SetDropItem(DragItem item)
+	{
+		_item = item;
+	}
+}
